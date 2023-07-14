8faec05ec9b3a8afbb28f7eda63e86b7485a49a6
B. Endres <endres@systopia.de>
2023-07-14 22:09:08 +0200

    added APIv3 Processor

---
+++ Civi/AIP/Processor/Api3.php
@@ -0,0 +1,141 @@
+<?php
+/*-------------------------------------------------------+
+| SYSTOPIA Automatic Input Processing (AIP) Framework    |
+| Copyright (C) 2023 SYSTOPIA                            |
+| Author: B. Endres (endres@systopia.de)                 |
++--------------------------------------------------------+
+| This program is released as free software under the    |
+| Affero GPL license. You can redistribute it and/or     |
+| modify it under the terms of this license which you    |
+| can read by viewing the included agpl.txt or online    |
+| at www.gnu.org/licenses/agpl.html. Removal of this     |
+| copyright header is strictly prohibited without        |
+| written permission from the original author(s).        |
++--------------------------------------------------------*/
+
+namespace Civi\AIP\Processor;
+
+use Civi\AIP\AbstractComponent;
+use CRM_Aip_ExtensionUtil as E;
+use \Civi as Civi;
+
+class Api3 extends Base
+{
+  /**
+   *
+   * @return void
+   */
+  /**
+   * Process the given record
+   *
+   * @param array $record
+   *
+   * @throws \Exception
+   */
+  public function processRecord($record)
+  {
+    // we're going to do three steps:
+    // 1) filter the parameters
+    $call_parameters = $record;
+    $call_parameters = $this->filterCallParameters($call_parameters);
+
+    // 2) map the parameters
+    $call_parameters = $this->mapCallParameters($call_parameters);
+
+    // 3) compile the API parameters
+    $entity = $this->getConfigValue('api_entity');
+    $action = $this->getConfigValue('api_action');
+    $hardcoded_values = (array) $this->getConfigValue('api_values');
+    $call_parameters = array_merge($call_parameters, $hardcoded_values);
+
+    // 4) Run the API call
+    \civicrm_api3($entity, $action, $call_parameters);
+
+    // do nothing here, override in implementation
+    $this->last_processed_record = $record;
+  }
+
+  /**
+   * @return ?array
+   *   get the last record processed by this processor
+   */
+  public function getLastProcessedRecord() : ?array
+  {
+    return $this->last_processed_record;
+  }
+
+  /**
+   * Filter the input parameters down to the ones
+   *
+   * @param array $parameters
+   *   the current call parameters
+   *
+   * @return array
+   */
+  protected function filterCallParameters(array $parameters) : array
+  {
+    // restrict record to allowed parameters
+    $positive_parameter_list = $this->getConfigValue('positive_parameter_list');
+    if (is_array($positive_parameter_list)) {
+      foreach ($positive_parameter_list as $field_name) {
+        if (!in_array($field_name, $positive_parameter_list)) {
+          unset($parameters[$field_name]);
+        }
+      }
+    }
+
+    // remove disallowed parameters
+    $negative_parameter_list = $this->getConfigValue('negative_parameter_list');
+    if (is_array($negative_parameter_list)) {
+      foreach ($negative_parameter_list as $field_name) {
+        unset($parameters[$field_name]);
+      }
+    }
+
+    return $parameters;
+  }
+
+  /**
+   * Filter the input parameters down to the ones
+   *
+   * @param array $parameters
+   *   the current call parameters
+   *
+   * @return array
+   */
+  protected function mapCallParameters(array $parameters) : array
+  {
+    // restrict record to allowed parameters
+    $parameter_mapping = $this->getConfigValue('parameter_mapping');
+    if (is_array($parameter_mapping)) {
+      foreach ($parameter_mapping as $old_field_name => $new_field_name) {
+        if ($old_field_name == $new_field_name) continue;
+
+        if (isset($parameters[$old_field_name])) {
+          $parameters[$new_field_name] = $parameters[$old_field_name];
+          unset($parameters[$old_field_name]);
+        }
+      }
+    }
+
+    // remove disallowed parameters
+    $negative_parameter_list = $this->getConfigValue('negative_parameter_list');
+    if (is_array($negative_parameter_list)) {
+      foreach ($negative_parameter_list as $field_name) {
+        unset($parameters[$field_name]);
+      }
+    }
+
+    return $parameters;
+  }
+
+  /**
+   * Return the type of the given component
+   *
+   * @return string
+   */
+  public function getType() : string
+  {
+    return E::ts("APIv3 Processor");
+  }
+}
