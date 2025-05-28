a31ac521950a7b865aab67395231bc4708f08353
johannes <margraf@systopia.de>
2025-05-28 14:00:06 +0200

    added config option to log api calls

--- Civi/AIP/Processor/Api3.php
+++ Civi/AIP/Processor/Api3.php
@@ -53,7 +53,7 @@ class Api3 extends Base
     // 4) Run the API call
     $this->log("Call API {$entity}.{$action} with parameters hash {$call_hash}", 'debug');
     $result = \civicrm_api3($entity, $action, $call_parameters);
-    if($this->getConfigValue('log/apicall') == '1') {
+    if (!empty($this->getConfigValue('log/apicall'))) {
       $this->log("Call API {$entity}.{$action} with parameters: ".var_export($call_parameters,true), 'debug');
       $this->log("Call API {$entity}.{$action} response: ".var_export($result,true), 'debug');
     }
