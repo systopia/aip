2a0611829a90736b588232140222a76b52a050e2
Jens Schuppe <schuppe@systopia.de>
2026-08-11 14:51:58 +0200

    Set version to 1.4.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'systopia/aip',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '2d317a0f9e070040a1742f6bccfbfe88565f545f',
+        'type' => 'civicrm-ext',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'systopia/aip' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '2d317a0f9e070040a1742f6bccfbfe88565f545f',
+            'type' => 'civicrm-ext',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
